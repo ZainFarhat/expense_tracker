d91a6611e1f54a22ad78a488fa6c2593dbbacba4
Zain Farhat <zainfarhat14@gmail.com>
2021-12-28 16:30:09 -0600

    navbar code

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  root to:"home#index" 
+
+  get "transactions", to: "transactions#index"
+
+  get "accounts", to: "accounts#index"
+
+  get "reports", to: "reports#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
